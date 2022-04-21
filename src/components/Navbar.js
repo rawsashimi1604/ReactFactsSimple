5964493d14e799268f968308d6a327daf6084f94
Gavin Loo <looweiren@gmail.com>
2022-04-22 01:23:05 +0800

    added styling for starter project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
 import React from "react"
+import logo from "../images/react_logo.png"
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav className="nav">
+            <div className="logo">
+                <img src={logo}></img>
+                <h1>ReactFacts</h1>
+            </div>
+
+            <div className="labels">
+                <h2>React Course - Project 1</h2>
+            </div>
+        </nav>
     );
 }
 
